4c054b6343fc9c5225d1995923799afd38aabe1e
leticiassaif <leticiassaif@gmail.com>
2026-06-15 20:01:25 -0300

    Initial commit

---
+++ src/main/java/br/ufma/springextensao/SpringExtensaoApplication.java
@@ -0,0 +1,13 @@
+package br.ufma.springextensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExtensaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExtensaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufma/springextensao/SpringExtensaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufma.springextensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExtensaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
