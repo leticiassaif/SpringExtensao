200b5c4c59620b15142eaa4f719688d010b5505d
leticiassaif <leticiassaif@gmail.com>
2026-06-15 22:32:58 -0300

    adicionado exemplo exemplo de como se deve implementar o service usado por geraldo em aula

--- src/main/java/br/ufma/springextensao/service/UsuarioService.java
+++ src/main/java/br/ufma/springextensao/service/UsuarioService.java
@@ -1,4 +1,25 @@
 package br.ufma.springextensao.service;
 
+import br.ufma.springextensao.model.Usuario;
+import br.ufma.springextensao.repository.UsuarioRepo;
+import org.jvnet.hk2.annotations.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    UsuarioRepo usuarioRepo;
+
+    // EXEMPLO USADO POR GERALDO:
+    /**
+     * Essa função busca um usuário por id
+     * @param id chave do usuário única
+     * @return nulo se não existir, Usuário na base se existir
+     */
+    public Usuario obterUsuarioPorId(Integer id) {
+        if (id == null) {
+            throw new IllegalArgumentException();
+        }
+        return usuarioRepo.findById(id).orElse(null);
+    }
 }
