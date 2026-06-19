daa8384509825b84a8551be252412e030dd20f5b
leticiassaif <leticiassaif@gmail.com>
2026-06-19 15:22:37 -0300

    feat: add find by email

--- src/main/java/br/ufma/springextensao/repository/UsuarioRepo.java
+++ src/main/java/br/ufma/springextensao/repository/UsuarioRepo.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepo
         extends JpaRepository<Usuario, Integer> {
+    Usuario findByEmail(String email);
 }
