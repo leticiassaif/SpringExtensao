c5557ca4b2ddb011fea026eb2d86b4097d6097e5
leticiassaif <leticiassaif@gmail.com>
2026-06-18 22:33:04 -0300

    feat: criação do repo de papeis

---
+++ src/main/java/br/ufma/springextensao/repository/PapelRepo.java
@@ -0,0 +1,8 @@
+package br.ufma.springextensao.repository;
+
+import br.ufma.springextensao.model.Papel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PapelRepo
+        extends JpaRepository<Papel, Integer> {
+}
