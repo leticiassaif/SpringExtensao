23373da9bf1e586208aa429ccec9378f531d3455
leticiassaif <leticiassaif@gmail.com>
2026-06-24 23:17:14 -0300

    feat: busca curso por id

--- src/main/java/br/ufma/springextensao/service/CursoService.java
+++ src/main/java/br/ufma/springextensao/service/CursoService.java
@@ -1,7 +1,19 @@
 package br.ufma.springextensao.service;
 
+import br.ufma.springextensao.model.Curso;
+import br.ufma.springextensao.repository.CursoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CursoService {
+    @Autowired
+    CursoRepo cursoRepo;
+
+    public Curso buscaPorId(Integer id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID inválido.");
+        }
+        return cursoRepo.findById(id).orElse(null);
+    }
 }
